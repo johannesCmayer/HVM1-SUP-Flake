a3165f4b32d26f2bfdad7a48a30abaceada6dbb7
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-08-09 15:09:55 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "HVM1 with DUP/SUP labels";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hvm-git = {
+      url = "github:HigherOrderCO/HVM1/dup_labels";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, hvm-git }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forEachSupportedSystem ({ pkgs }: rec {
+        hvm1_git = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "hvm1-sup";
+          version = "1.0.16";
+
+          src = hvm-git;
+          cargoHash = "sha256-W7tniTFmzFS3AdEdQyqBDoV1w6PzdIu4iv66PGaQUHQ=";
+          doCheck = true;
+
+          cargoPatches = [
+            # a patch file to add/update Cargo.lock in the source code
+            ./add-Cargo.lock.patch
+          ];
+
+          RUSTC_BOOTSTRAP = true;
+          meta = with pkgs.lib; {
+            description = "A massively parallel, optimal functional runtime in Rust";
+            mainProgram = "hvm1";
+            homepage = "https://github.com/higherorderco/hvm1";
+          };
+        };
+      });
+    };
+}
